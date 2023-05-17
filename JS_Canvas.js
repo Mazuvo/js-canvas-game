61730e77ac88cf0d3a58b94943856884f63c6cdf
Mazurka Vojtěch <mazuvo@delta-studenti.cz>
2023-05-17 16:02:29 +0200

    sbirani_pointu

--- JS_Canvas.js
+++ JS_Canvas.js
@@ -11,6 +11,7 @@ canvas.height = window_height;
 canvas.style.background = "#ff8";
 
 var ob_count = 0;
+var coin_count = 0;
 var tile_size = 50;
 var direction = null;
 var counter = 0;
@@ -29,14 +30,15 @@ var Map = [
 
 
 class Circle {
-    constructor(radius, color, text, map, ){
+    constructor(radius, color){
 
         this.Xpos = 0;
         this.Ypos = 0;
         this.Radius = radius;
         this.Color = color;
-        this.text = text;
-        this.Map = map;
+
+        this.SpawnX = 0;
+        this.SpawnY = 0;
 
     }
     spawn(){
@@ -48,7 +50,8 @@ class Circle {
 
         } while(Map[this.Ypos][this.Xpos] != 0)
 
-        this.Map[this.Ypos][this.Xpos] = 2;
+        this.SpawnX = this.Xpos;
+        this.SpawnY = this.Ypos;
 
     }
     draw(context){
@@ -60,8 +63,6 @@ class Circle {
         context.textBaseline = "middle";
         context.font = "20px Arial";
 
-        context.fillText(this.text, (tile_size * (this.Xpos + 0.5)), (tile_size * (this.Ypos + 0.5)));
-
         context.lineWidth = 5;
         context.arc((tile_size * (this.Xpos + 0.5)), (tile_size * (this.Ypos + 0.5)), this.Radius, 0, Math.PI *2, false);
         context.stroke();
@@ -72,20 +73,20 @@ class Circle {
         this.draw(context);
     }
 }
-
 class Enemi {
-    constructor(radius, color, map, speed ){
+    constructor(radius, color, speed ){
 
         this.Xpos = null;
         this.Ypos = null;
+
         this.Radius = radius;
         this.Color = color;
-        this.Map = map;
         this.Speed = speed;
 
-        
         this.dx = 0;
         this.dy = 0;
+        
+        this.Rnd = RandomNum(4);
 
     }
     spawn(){
@@ -95,18 +96,17 @@ class Enemi {
             this.Xpos = RandomNum(Map[0].length -1);
             this.Ypos = RandomNum(Map.length -1);
 
-        } while(Map[this.Ypos][this.Xpos] != 0 || player.Map[this.Ypos][this.Xpos] == 2)
+        } while(Map[this.Ypos][this.Xpos] != 0 || this.Ypos == player.SpawnY && this.Xpos == player.SpawnX)
 
 
     }
     draw(context){
         context.beginPath();
 
-        context.strokeStyle = this.Color;
+        context.fillStyle = this.Color;
 
-        context.lineWidth = 5;
         context.arc((tile_size * (this.Xpos + 0.5)), (tile_size * (this.Ypos + 0.5)), this.Radius, 0, Math.PI *2, false);
-        context.stroke();
+        context.fill();
         context.closePath();
     }
     update() {
@@ -115,26 +115,24 @@ class Enemi {
         
         if(Map[this.Ypos + this.dy][this.Xpos] == 0 && this.dy != 0) {}
         else if(Map[this.Ypos][this.Xpos + this.dx] == 0 && this.dx != 0) {}
-        else Rnd = RandomNum(4);
+        else this.Rnd = RandomNum(4);
     }
     Movement() { //dx + Xpos
 
         //up
-        if (Rnd == 1) this.dy = -1;
+        if (this.Rnd == 1) this.dy = -1;
         //down
-        else if (Rnd == 2) this.dy = 1;
+        else if (this.Rnd == 2) this.dy = 1;
         //left
-        else if (Rnd == 3) this.dx = -1;
+        else if (this.Rnd == 3) this.dx = -1;
         //right
-        else if (Rnd == 4) this.dx = 1;
+        else if (this.Rnd == 4) this.dx = 1;
 
         if(Map[this.Ypos + this.dy][this.Xpos] == 0 && this.dy != 0) this.Ypos = this.Ypos + this.dy;
         else if(Map[this.Ypos][this.Xpos + this.dx] == 0 && this.dx != 0) this.Xpos = this.Xpos + this.dx;
-        else Rnd = RandomNum(4);
+        else this.Rnd = RandomNum(4);
     }
 } 
-
-
 class box {
     constructor(xpos, ypos, height, width, color){
 
@@ -161,37 +159,73 @@ class box {
     }
 
 }
+class Coins {
+    constructor(xpos, ypos){
+        
+        this.Xpos = xpos;
+        this.Ypos = ypos;
+        this.Picked = false;
+    }
+    draw(context){
+
+        context.beginPath();
+
+        if (this.Picked == false) context.fillStyle = "pink";
+        else context.fillStyle = "#ff8";
+        
+
+        context.arc((tile_size * (this.Xpos + 0.5)), (tile_size * (this.Ypos + 0.5)), 5, 0, Math.PI *2, false);
+        context.fill();
+        context.closePath();
+    }
+    update(){
+        this.draw(context);
+
+        if (this.Xpos == player.Xpos && this.Ypos == player.Ypos) this.Picked = true;
+
+    }
+
+}
 
 let RandomNum = function(num) {
 
     var Random = Math.floor(Math.random() * num) + 1;
     return Random; 
 }
-var Rnd = RandomNum(4);
 
 
-var objects = new box();
+var object_box = new box();
+var object_coin = new Coins(); 
 
 function SetMap(Map) {
 
     for (let i = 0; i < Map.length; i++) {
         for (let j = 0; j < Map[0].length; j++) {
             if (Map[i][j] == 1) {
-                objects[ob_count] = new box(j*tile_size, i*tile_size, tile_size, tile_size, "orange");
-                objects[ob_count].draw(context);
+                object_box[ob_count] = new box(j*tile_size, i*tile_size, tile_size, tile_size, "orange");
+                object_box[ob_count].draw(context);
                 ob_count++;
+            }
+            if (Map[i][j] == 0 && i != player.Ypos || Map[i][j] == 0 && j != player.Xpos) {
+                object_coin[coin_count] = new Coins( j, i);
+                object_coin[coin_count].draw(context);
+                coin_count++;
             }
         }
     }
 }
 
-player = new Circle(25, "black", "P", Map, 1);
+player = new Circle(22, "black", "P", 1);
 player.spawn();
 player.draw(context);
 
-enemi = new Enemi(25, "black", Map);
-enemi.spawn();
-enemi.draw(context);
+enemi2 = new Enemi(22, "blue");
+enemi2.spawn();
+enemi2.draw(context);
+
+enemi1 = new Enemi(22, "red");
+enemi1.spawn();
+enemi1.draw(context);
 
 
 SetMap(Map);
@@ -207,23 +241,27 @@ let updateCircle = function() {
 
     context.clearRect(0, 0, window_width, window_height);
 
+    for (let i = 0; i < ob_count; i++) {
+        object_box[i].update();
+    }
+    for (let i = 0; i < coin_count; i++) {
+        object_coin[i].update();
+    }
+
     player.update();
-    enemi.update();
+
+    enemi2.update();
+    enemi1.update();
 
     if (counter == 100) {
-        enemi.Movement();
+        enemi1.Movement();
+        enemi2.Movement();
         counter = 0;
     }
     else counter++;
-        
-
-    for (let i = 0; i < ob_count; i++) {
-        objects[i].update();
-    }
-
 }
-
 updateCircle();
+
 function checkKey(e) {
 
     e = e || window.event;
