12ff7252a8e04102293790f4958593bc0fc672d8
Mazurka Vojtěch <mazuvo@delta-studenti.cz>
2023-05-22 12:21:52 +0200

    levelovani

--- JS_Canvas.js
+++ JS_Canvas.js
@@ -17,7 +17,11 @@ var tile_size = 50;
 var direction = null;
 var counter = 0;
 var collected = 0;
-var roundend = false;
+var roundendW = true;
+var roundendL = false;
+var score = 0;
+
+var Colors = ["red", "blue", "green", "purple"];
 
 var Map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -74,10 +78,6 @@ class Circle {
     update() {
 
         this.draw(context);
-        
-        WinEnd(collected);
-
-        LoseEnd();
     }
 }
 class Enemi {
@@ -119,9 +119,11 @@ class Enemi {
 
         this.draw(context);
         
-        if(Map[this.Ypos + this.dy][this.Xpos] != 1 && this.dy != 0) {}
-        else if(Map[this.Ypos][this.Xpos + this.dx] != 1 && this.dx != 0) {}
+        if(Map[this.Ypos + this.dy][this.Xpos] == 0 && this.dy != 0) {}
+        else if(Map[this.Ypos][this.Xpos + this.dx] == 0 && this.dx != 0) {}
         else this.Rnd = RandomNum(4);
+
+        if(Map[this.Ypos][this.Xpos] == 0) Map[this.Ypos][this.Xpos] = 3;
     }
     Movement() { //dx + Xpos
 
@@ -134,8 +136,14 @@ class Enemi {
         //right
         else if (this.Rnd == 4) this.dx = 1;
 
-        if(Map[this.Ypos + this.dy][this.Xpos] != 1 && this.dy != 0) this.Ypos = this.Ypos + this.dy;
-        else if(Map[this.Ypos][this.Xpos + this.dx] != 1 && this.dx != 0) this.Xpos = this.Xpos + this.dx;
+        if(Map[this.Ypos + this.dy][this.Xpos] == 0 && this.dy != 0) {
+            if(Map[this.Ypos][this.Xpos] == 3) Map[this.Ypos][this.Xpos] = 0;
+            this.Ypos = this.Ypos + this.dy;
+        }
+        else if(Map[this.Ypos][this.Xpos + this.dx] == 0 && this.dx != 0) {
+            if(Map[this.Ypos][this.Xpos] == 3) Map[this.Ypos][this.Xpos] = 0;
+            this.Xpos = this.Xpos + this.dx;
+        }
         else this.Rnd = RandomNum(4);
     }
 } 
@@ -194,16 +202,30 @@ class Coins {
 
 }
 
-let RandomNum = function(num) {
+var object_box = new box();
+var object_coin = new Coins();
+var object_enemi = new Enemi();
+
+Levels();
+
+document.addEventListener('keyup', checkKey);
+
+updateCircle();
+
+
+
+function RandomNum(num) {
 
     var Random = Math.floor(Math.random() * num) + 1;
     return Random; 
 }
 
+function Score(collected, score) {
+    var a = collected - 1;
 
-var object_box = new box();
-var object_coin = new Coins();
-var object_enemi = new Enemi();
+    score = score + (collected - a);
+    return score;
+}
 
 function SetMap(Map) {
 
@@ -214,7 +236,8 @@ function SetMap(Map) {
                 object_box[ob_count].draw(context);
                 ob_count++;
             }
-            if (Map[i][j] == 0 && i != player.Ypos || Map[i][j] == 0 && j != player.Xpos) {
+            if (Map[i][j] != 1 && i != player.Ypos || Map[i][j] != 1 && j != player.Xpos) {
+                Map[i][j] = 0;
                 object_coin[coin_count] = new Coins( j, i);
                 object_coin[coin_count].draw(context);
                 coin_count++;
@@ -223,63 +246,32 @@ function SetMap(Map) {
     }
 }
 
-player = new Circle(22, "black");
-player.spawn();
-player.draw(context);
+function Levels() {
+    if (roundendW == true && roundendL == false) {
 
-CreateEnemi(2);
+        roundendW = false;
 
-SetMap(Map);
+        player = new Circle(22, "black");
+        player.spawn();
+        player.draw(context);
 
-let updateCircle = function() {
-    requestAnimationFrame(updateCircle);
 
-    context.clearRect(0, 0, window_width, window_height);
+        enemi_count = 0;
+        ob_count = 0;
+        coin_count = 0;
+        collected = 0;
 
-    for (let i = 0; i < ob_count; i++) {
-        object_box[i].update();
+        CreateEnemi(3, Colors);
+
+        SetMap(Map);
     }
-    for (let i = 0; i < coin_count; i++) {
-        object_coin[i].update();
-        if (object_coin[i].Picked == true && object_coin[i].Checked == false) {
-            collected++;
-            object_coin[i].Checked = true;
-        }
-    }
-
-    context.font = "30px Arial";
-    context.fillStyle = "Black";
-    context.fillText(collected, tile_size * 0.5, tile_size * 0.5);
-    context.fillText(coin_count, tile_size * 1.5, tile_size * 0.5);
-
-    player.update();
-    
-    if (roundend == false) {
-        for (let i = 0; i < enemi_count; i++) object_enemi[i].update();
-    }
-    else {
-        for (let i = 0; i < enemi_count; i++) {
-            if (object_enemi[i].Xpos == player.Xpos && object_enemi[i].Ypos == player.Ypos ) {
-                object_enemi[i].update();
-            }
-        }
-    }
-
-    if (counter == 100 && roundend == false) {
-        for (let i = 0; i < enemi_count; i++) {
-            object_enemi[i].Movement();
-        }   
-        counter = 0;
-    }
-    else counter++;
 }
-updateCircle();
 
 function checkKey(e) {
 
     e = e || window.event;
 
-    if (roundend == false) {
+    if (roundendW == false && roundendL == false) {
         
         if (e.keyCode === 87 && Map[player.Ypos-1][player.Xpos] != 1) {
             direction = "up"; //up
@@ -299,11 +291,10 @@ function checkKey(e) {
         }
     }
 }
-document.addEventListener('keyup', checkKey);
 
 function WinEnd(collected) {
-    if (collected == coin_count) {
-        roundend = true;
+    if (collected == coin_count && roundendL == false) {
+        roundendW = true;
 
         context.font = "30px Arial";
         context.fillStyle = "Black";
@@ -316,7 +307,7 @@ function LoseEnd() {
     for (let i = 0; i < enemi_count; i++) {
         
         if (object_enemi[i].Xpos == player.Xpos && object_enemi[i].Ypos == player.Ypos) {
-            roundend = true;
+            roundendL = true;
     
             context.font = "30px Arial";
             context.fillStyle = "Black";
@@ -327,11 +318,11 @@ function LoseEnd() {
     }
 }
 
-function CreateEnemi(quantity) {
+function CreateEnemi(quantity, Colors) {
 
     for (let i = 0; i < quantity; i++) {
 
-        object_enemi[i] = new Enemi((tile_size/2)-3, "red");       
+        object_enemi[i] = new Enemi((tile_size/2)-3, Colors[i]);       
         object_enemi[i].spawn();
         object_enemi[i].draw(context);
         
@@ -339,7 +330,56 @@ function CreateEnemi(quantity) {
     }
 }
 
+function updateCircle() {
+    requestAnimationFrame(updateCircle);
 
+    Levels();
 
+    context.clearRect(0, 0, window_width, window_height);
 
+    for (let i = 0; i < ob_count; i++) {
+        object_box[i].update();
+    }
+    for (let i = 0; i < coin_count; i++) {
+        object_coin[i].update();
+        if (object_coin[i].Picked == true && object_coin[i].Checked == false) {
+            collected++;
 
+            context.font = "30px Arial";
+            context.fillStyle = "Black";
+            score = Score(collected, score);
+            
+            object_coin[i].Checked = true;
+        }
+    }
+
+    context.font = "30px Arial";
+    context.fillStyle = "Black";
+    context.fillText(collected, tile_size * 0.5, tile_size * 0.5);
+    context.fillText(coin_count, tile_size * 1.5, tile_size * 0.5);
+    context.fillText("Score: " + score, tile_size * (Map[0].length/2), tile_size * 0.5);
+
+    player.update();
+    
+    if (roundendL == false && roundendW == false) {
+        for (let i = 0; i < enemi_count; i++) object_enemi[i].update();
+    }
+    else {
+        for (let i = 0; i < enemi_count; i++) {
+            if (object_enemi[i].Xpos == player.Xpos && object_enemi[i].Ypos == player.Ypos ) {
+                object_enemi[i].update();
+            }
+        }
+    }
+
+    if (counter == 100 && roundendL == false && roundendW == false) {
+        for (let i = 0; i < enemi_count; i++) {
+            object_enemi[i].Movement();
+        }   
+        counter = 0;
+    }
+    else counter++;
+
+    WinEnd(collected);
+    LoseEnd();
+}
